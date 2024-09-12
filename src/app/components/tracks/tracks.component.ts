f3652e98094b13b3e47a3bd5b0763ec5a371bee8
Matthew Krauskopf <matthew.krauskopf@outlook.com>
2024-09-12 15:26:39 -0400

    Project skeleton

---
+++ src/app/components/tracks/tracks.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tracks',
+  standalone: true,
+  imports: [],
+  templateUrl: './tracks.component.html',
+  styleUrl: './tracks.component.scss'
+})
+export class TracksComponent {
+
+}
